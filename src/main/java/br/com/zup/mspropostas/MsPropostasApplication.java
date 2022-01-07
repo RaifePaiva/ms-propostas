1ef5c4a609533a12e83a730fecc93648ba6394c9
RaifePaiva <raife.tec@gmail.com>
2022-01-07 00:09:42 -0300

    Criação do projeto base sem dependencias

---
+++ src/main/java/br/com/zup/mspropostas/MsPropostasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.mspropostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPropostasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/mspropostas/MsPropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.mspropostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
